739ee2381ac862262fb6ad919fb9d81278c27171
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-25 12:13:45 +0530

    Contacts UC1

---
+++ AdressBookUser.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+
+public class AdressBookUser {
+
+	ArrayList<Contactsuser> contacts = new ArrayList<>();
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System!");
+	}
+
+}
