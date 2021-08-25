3a3f2cd82b1844977677ddd829263b2fdbafa7c8
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-25 13:43:26 +0530

    Able to Edit User

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,11 +4,18 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Address book program..");
+		System.out.println("Welcome to Address book");
 		AdressBookUser Entery = new AdressBookUser();
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("How Many entry you want to make this time");
 		Entery.addContact();
+
+		System.out.println("you to make any changes in enter type yes or no");
+		String input = sc.next();
+		if (input.equals("yes")) {
+
+			Entery.editContact();
+		}
 		sc.close();
 	}
 }
